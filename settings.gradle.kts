b0315109ea8652755da1cce25eb57c7161c6fdb7
AULA F.DE ING.DE SISTEMAS E INF <AULAFISI@UNMSM.pe>
2023-08-23 21:24:00 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "18200218_20200219_HappyBirthday"
+include(":app")
